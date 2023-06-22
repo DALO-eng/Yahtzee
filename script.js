7e51f304a86077803ddc1cde44c58b4107dea67b
Daniel Leon <dalobusiness01@gmail.com>
2023-06-22 11:46:08 -0500

    [ADD] Added rolling dices feature

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const rollDice = function () {
+  return 1 + Math.floor(Math.random() * 6);
+};
+
+const rollDices = function (n) {
+  const dices = [];
+  for (let i = 0; i < n; i++) {
+    dices.push(rollDice());
+  }
+  return dices;
+};
+
+console.log(rollDices(5));
