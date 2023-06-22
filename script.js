49b47b1b9234331658d981d67cdafb2cc4b3620b
Daniel Leon <dalobusiness01@gmail.com>
2023-06-22 15:15:37 -0500

    [ADD] Added throwing phase feature

--- script.js
+++ script.js
@@ -12,4 +12,46 @@ const rollDices = function (n) {
   return dices;
 };
 
-console.log(rollDices(5));
+const getNumberFromString = function (selectedDices) {
+  return selectedDices.split(",").map((x) => Number(x));
+};
+
+const checkValidDiceSelection = function (dices, selectedInput) {
+  const dicesRolled = [...dices];
+  console.log(dicesRolled, selectedInput);
+  const selectedDices = [];
+  for (let i = 0; i < selectedInput.length; i++) {
+    if (dicesRolled.includes(selectedInput[i])) {
+      selectedDices.push(selectedInput[i]);
+      dicesRolled.splice(dicesRolled.indexOf(selectedInput[i]), 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const throwingPhase = function () {
+  let playerDices = [];
+  let dicesLeft = 5;
+  for (let i = 0; i < 3; i++) {
+    let validation = false;
+    const currentDices = rollDices(dicesLeft);
+    while (!validation) {
+      let keepingDices = prompt(
+        `Enter the dices you wanna keep: ${currentDices} \nEx:4,5,6`
+      );
+      const selectedDices = getNumberFromString(keepingDices);
+      if (checkValidDiceSelection(currentDices, selectedDices)) {
+        playerDices = playerDices.concat(selectedDices);
+        dicesLeft -= playerDices.length;
+        validation = true;
+        console.log(playerDices);
+      } else alert("Wrong input! Please try again");
+    }
+    if (playerDices.length === 5) break;
+  }
+  alert(`Your dices scores: ${playerDices}`);
+};
+
+throwingPhase();
