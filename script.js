bd95b02c6bef9b1e784fb6b00f280d02b7ae26b4
DALO <dalobusiness01@gmail.com>
2023-07-25 15:36:58 -0500

    [FEAT] Add checked/unchecked dice logic

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 "use strict";
 
+const playBtnEl = document.getElementById("play-btn");
 const diceTableEl = document.querySelector(".dice-table");
 const dicesEl = document.querySelectorAll(".dice");
-const playBtnEl = document.querySelector(".play-button");
+const dicesInputEl = document.querySelectorAll(".dice-input");
 const rollingBtnsEl = document.querySelector(".rolling-section");
+const skipBtnEl = document.getElementById("skip-btn");
+const rerollBtnEl = document.getElementById("reroll-btn");
 
 let playing, rollsLeft;
 
@@ -27,10 +30,36 @@ const rollDices = function (n) {
   return dices;
 };
 
+const isDiceUnchecked = function (index) {
+  return !dicesInputEl[index].checked;
+};
+
+const getUncheckedDicesLength = function () {
+  let diceInputsArr = Array.from(dicesInputEl);
+  return diceInputsArr.filter((input) => !input.checked).length;
+};
+
+const getDiceImg = function (dices) {
+  let currentNewDiceIndex = 0;
+  dicesEl.forEach((dice, i) => {
+    if (isDiceUnchecked(i)) {
+      dice.src = `./imgs/dice-${dices[currentNewDiceIndex]}.png`;
+      currentNewDiceIndex++;
+    }
+  });
+};
+
 playBtnEl.addEventListener("click", () => {
   beginGame();
   const dices = rollDices(5);
-  dicesEl.forEach((dice, i) => {
-    dice.src = `./imgs/dice-${dices[i]}.png`;
-  });
+  getDiceImg(dices);
+});
+
+rerollBtnEl.addEventListener("click", () => {
+  const dices = rollDices(getUncheckedDicesLength());
+  getDiceImg(dices);
+});
+
+skipBtnEl.addEventListener("click", () => {
+  console.log(getUncheckedDicesLength());
 });
